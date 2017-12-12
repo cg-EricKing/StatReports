c85b13aae6a93be8676308b27b3a93f65aff9c3e
Eric King <cg100146@comporium.com>
2017-12-12 12:08:00 -0500

    Added stats function to return 2d array with report data

--- StatReports.js
+++ StatReports.js
@@ -3,6 +3,7 @@
 
 // TODAY, YESTERDAY, LAST_7_DAYS, THIS_WEEK_SUN_TODAY, LAST_WEEK, LAST_14_DAYS, LAST_30_DAYS, LAST_BUSINESS_WEEK, LAST_WEEK_SUN_SAT, THIS_MONTH, LAST_MONTH, ALL_TIME
 // Add to this script - 
+    // Stats function to clean up the code - less variables and repetition
     // Tie in a spreadsheet (multiple sheets for multiple stats ranges if requested)
         // Possibility of a select menu in spreadsheet to fire script and show data
         // If not a select menu, show the ranges to choose from and have it be enetered => then a function will grab that and enter it in the getStatsFor() method.
@@ -28,12 +29,42 @@ function main() {
     var sheet = spreadsheet.getSheets()[0];
 
     var currentAccount = AdWordsApp.currentAccount();
+    var currentAccountName = currentAccount.getName();
+    var accountNameRange = sheet.getRange(row, headers.accountName);
+    var accountNameArray = [[currentAccountName]];
+    accountNameRange.setValues(accountNameArray);
+
     var data = currentAccount.getCustomerId() + " " + currentAccount.getName() + " " + currentAccount.getTimeZone();
     // =================================== CHANGE THIS FIELD WITH A TIMELINE FROM ABOVE =========================================
     // ==========================================================================================================================
                                         var stats = currentAccount.getStatsFor('LAST_MONTH');
     // ==========================================================================================================================
     // ==========================================================================================================================
+    
+    // function to take in date range and return a 2d array of data
+    function getStats(range) {
+
+        var statsRange = currentAccount.getStatsFor(range);
+        var clicks = statsRange.getClick();
+        var impressions = statsRange.getImpressions();
+        var ctr = statsRange.getCtr();
+        var cost = statsRange.getCost();
+        var avgCpm = statsRange.getAverageCpm();
+        var cpc = statsRange.getAverageCpc();
+        var conversions = statsRange.getConversions();
+        var conversionRate = statsRange.getConversionRate();
+
+        var statsValues = [[clicks,impressions,ctr,cost,avgCpm,cpc,conversions,conversionRate]];
+
+        return statsValues;
+
+    }
+
+    var reportRange = sheet.getRange('C2:J2');
+    var currentReport = getStats("THIS_MONTH");
+    reportRange.setValues(currentReport);
+
+    
     var specificStats = "Stats for last Month - " 
     + "Clicks: " + stats.getClicks() + ", " 
     + "AverageCpm: " + stats.getAverageCpm() + ", " 
